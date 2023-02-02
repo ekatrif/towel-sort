a0a46bebef6ad144d54d656c94d6a61701f56a71
ekatrif <katiapervak@gmail.com>
2023-02-02 09:36:01 +0100

    feat: task is done without errors

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const result = [];
+  if (!matrix) {return result}
+  matrix.forEach(function(array,index) {
+    if (index%2===0) {
+      for (let i=0;i<array.length;i++) {
+      result.push(array[i]);
+      }
+    } else {
+      for (let i=array.length-1;i>=0;i--) {
+        result.push(array[i]);
+        }
+    }
+  })
+  return result;
 }
